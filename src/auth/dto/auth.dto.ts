303bbc948a139b9d337a41c8342f228b7f2db23d
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2024-01-11 15:01:28 +0700

    🔷 input sanitation

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,6 +1,14 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class AuthDto {
+  @IsString()
+  @IsNotEmpty()
+  first_name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  last_name: string;
+
   @IsEmail()
   @IsNotEmpty()
   email: string;
