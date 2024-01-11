c08546416994a36bf006fa60fc32df542fd77598
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2024-01-11 10:59:35 +0700

    ❌ remove unnecessary code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthService } from './auth/auth.service';
-import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AuthController, AppController],
-  providers: [AuthService, AppService],
+  imports: [AuthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
-  imports: [],
   controllers: [AuthController],
-  providers: [AuthModule],
+  providers: [AuthService],
 })
 export class AuthModule {}
