d38132b4a43f5deecd5febfe67a3b84604b1dbb9
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2024-01-11 20:55:21 +0700

    🔷 change dotenv -> @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
