c43bf445e9b5ac72208b59e558f58517adc14f49
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2024-01-13 09:08:06 +0700

    🚩 fix import error JWT Strategy

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-strategy';
 import { ExtractJwt } from 'passport-jwt';
+import { Strategy } from 'passport-local';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy, 'for-jwt') {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(config: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
