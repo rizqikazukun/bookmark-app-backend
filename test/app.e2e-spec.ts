c22a1ceee46c7af8ee4e57f6b41eff0d4f5d3ad0
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2024-01-13 18:59:56 +0700

    ✅ setup prisma for testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,12 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('App-e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
+
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -17,6 +20,8 @@ describe('App-e2e', () => {
     );
 
     await app.init();
+    prisma = await app.get(PrismaService);
+    await prisma.cleanDb();
   });
 
   afterAll(() => {
